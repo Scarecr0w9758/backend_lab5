88be536d08d033fbc8a58ecc5edb364a4da66ce1
Matvey.d <Matway.demin@yandex.ru>
2023-05-04 11:42:24 +0500

    feat: added imports for sqlalchemy

--- product/mainImports.py
+++ product/mainImports.py
@@ -1,6 +1,8 @@
 import sys
 sys.path.append('..')
-from config.database import connection
+from config.database import Product, engine
+from sqlalchemy.orm import sessionmaker
 import pymysql
 from flask import Flask,jsonify
 app=Flask(__name__)
+Session= sessionmaker (autoflush=False,bind=engine)
