3fc4f8fa56d862f8f14006ea71a795ac8fe02e5d
Matvey.d <Matway.demin@yandex.ru>
2023-05-06 11:41:23 +0500

    faet: add read-method (getALL) using JSON

--- product/read.py
+++ product/read.py
@@ -2,7 +2,15 @@ from mainImports import *
 # TODO: pagination
 def getProductList():
     with Session(autoflush=True, bind=engine) as db:
-        product_query=db.query(Product).all()
-        # Доделавй цикл фор ин в котором вызывай чтение по ID
+        product_query_all=db.query(Product).all()
+        to_json_array=[]
+        for element in product_query_all:
+            tmp_json={}
+            tmp_json['id'] = element.id
+            tmp_json['name'] = element.name
+            tmp_json['description'] = element.description
+            tmp_json['price'] = element.price
+            tmp_json['category_id'] = element.category_id
+            to_json_array.append(tmp_json)
         db.commit()
-        return product_query[0].name
+        return to_json_array
