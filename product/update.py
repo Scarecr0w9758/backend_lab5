866bcd9e756d665f028a5e45507928c01ed84929
Matvey.d <Matway.demin@yandex.ru>
2023-05-06 11:41:54 +0500

    feat: add update w/fixed elems

--- product/update.py
+++ product/update.py
@@ -1,28 +1,17 @@
 from datetime import datetime
+from read_one import getProductById
 from mainImports import *
-def updateProduct(productId, productName, productDescription, productPrice, productCategoryId):
-    try:
-        MODIFIED_DATE=datetime.now()
-        print("mDate",MODIFIED_DATE)
-        print('success connected')
-        try:
-            with app.app_context():
-                with connection.cursor() as cursor:
-                    update_query=f"UPDATE `PRODUCTS`\
-                    SET NAME='{productName}',\
-                    DESCRIPTION='{productDescription}',\
-                    PRICE='{productPrice}',\
-                    CATEGORY_ID='{productCategoryId}',\
-                    MODIFIED='{MODIFIED_DATE}'\
-                    WHERE ID ='{productId}';"
-                    cursor.execute(update_query)
-                    connection.commit()
-                    rows=cursor.fetchall()
-                    return jsonify(rows)
-        except Exception as ex:
-            print("connection refused with: ", ex)
 
-        finally:
-            connection.close()
-    except Exception as ex:
-        print("connection refused with: ", ex)
+def updateProduct(productId, productName="", productDescription="", productPrice=0, productCategoryId=0):
+    with Session(autoflush=True, bind=engine) as db:
+        productQuery=db.query(Product).get(productId)
+        if (productName!=""):
+            productQuery.name = productName
+        if (productDescription!=""):
+            productQuery.description = productDescription
+        if (productPrice!=0):
+            productQuery.price= productPrice
+        if (productCategoryId!=0):
+            productQuery.category_id = productCategoryId
+        db.commit()
+        return getProductById(productId)
