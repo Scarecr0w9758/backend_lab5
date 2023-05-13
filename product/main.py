63d7d2bb34fe49ab67d801a956f47d66fb94a937
Matvey.d <Matway.demin@yandex.ru>
2023-05-13 11:37:54 +0500

    understand the CURL-rocessing but still haven't PUT, but dlete is OK

--- product/main.py
+++ product/main.py
@@ -3,7 +3,7 @@ from read_one import getProductById
 from create import createProduct 
 from delete import deleteProductById 
 from update import updateProduct
-from flask import Flask, Request, jsonify
+from flask import Flask, Request, jsonify, request
 import requests
 
 # Все методы (импортированные) настроены и готовы к работе
@@ -23,12 +23,22 @@ def hello():
 def getProducts():
     return getProductList()
 
+@app.route('/products/create',methods=['POST'])
+def createProduct():
+    return request.get_json()["name"]
+
 
 # разобраться почему не работает делит и пут, а также добавить прокидку параметров элементов 
-@app.route('/products/<id>',methods=["GET" ,"POST"])
-def postupdateProduct(id):
-    return updateProduct(3,"dahell")
+@app.route('/products/<id>',methods=["GET" ,"DELETE", "POST"])
+def changeOneProduct(id):
+    request_data=request.get_json()
+    if request.method=="DELETE":
+        return deleteProductById(id)    
+    elif request.method=="POST":
+        return updateProduct(id,request_data["name"])    
+    elif request.method=="GET":
+        return getProductById(id)    
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=8080)
