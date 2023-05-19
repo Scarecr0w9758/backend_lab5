2a7bef41b3db4f65e1507d84113182e1c6dae641
Scarecr0w9758 <132538423+Scarecr0w9758@users.noreply.github.com>
2023-05-19 14:22:24 +0500

    feat: fix parsing (use `  "\  ` in CURL

--- product/main.py
+++ product/main.py
@@ -29,13 +29,17 @@ def createProduct():
 
 
 # разобраться почему не работает делит и пут, а также добавить прокидку параметров элементов 
-@app.route('/products/<id>',methods=["GET" ,"DELETE", "POST"])
+@app.route('/products/<id>',methods=["GET" ,"DELETE", "PUT"])
 def changeOneProduct(id):
     request_data=request.get_json()
     if request.method=="DELETE":
         return deleteProductById(id)    
-    elif request.method=="POST":
-        return updateProduct(id,request_data["name"])    
+    elif request.method=="PUT":
+        return updateProduct(id,
+        productName = request_data["name"],
+        productDescription = request_data["description"],
+        productPrice = request_data["price"],
+        productCategoryId= request_data["category_id"])    
     elif request.method=="GET":
         return getProductById(id)    
 
