b77851f2408b809c9d910cc44479672583aa3e10
Matvey_Demin <m.demin@fact.digital>
2023-05-19 19:02:47 +0500

    Feat: fix PUT and add POST_creation

--- product/main.py
+++ product/main.py
@@ -24,14 +24,19 @@ def getProducts():
     return getProductList()
 
 @app.route('/products/create',methods=['POST'])
-def createProduct():
-    return request.get_json()["name"]
+def createNewProduct():
+    request_data=request.get_json()
+    return createProduct(id,
+    productName = request_data["name"],
+    productDescription = request_data["description"],
+    productPrice = request_data["price"],
+    productCategoryId= request_data["category_id"])
 
 
 # разобраться почему не работает делит и пут, а также добавить прокидку параметров элементов 
 @app.route('/products/<id>',methods=["GET" ,"DELETE", "PUT"])
 def changeOneProduct(id):
-    request_data=request.get_json()
+    request_data=request.get_json(force=True)
     if request.method=="DELETE":
         return deleteProductById(id)    
     elif request.method=="PUT":
@@ -45,4 +50,4 @@ def changeOneProduct(id):
 
 
 if __name__ == '__main__':
-    app.run(port=8080)
+    app.run(port=5050)
